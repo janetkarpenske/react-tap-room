89bb18ace6df9bf4a7d704c57982c88b8a342b3a
Janet Karpenske <janetkarpenske@gmail.com>
2021-02-22 11:46:15 -0800

    Updates

---
+++ precache-manifest.6c2710f166af32f835bf5de595dce1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a30e55f0634b66c35f579175b8662ed",
+    "url": "/react-tap-room-2/index.html"
+  },
+  {
+    "revision": "acbc91f401dde6438ce0",
+    "url": "/react-tap-room-2/static/css/main.66fc89a9.chunk.css"
+  },
+  {
+    "revision": "23f243e138c8d53d2066",
+    "url": "/react-tap-room-2/static/js/2.7a1bc4d0.chunk.js"
+  },
+  {
+    "revision": "edbf634c195cfa514b68",
+    "url": "/react-tap-room-2/static/js/3.4308dc24.chunk.js"
+  },
+  {
+    "revision": "acbc91f401dde6438ce0",
+    "url": "/react-tap-room-2/static/js/main.3c246af0.chunk.js"
+  },
+  {
+    "revision": "fd9541afdf2f157ab86e",
+    "url": "/react-tap-room-2/static/js/runtime-main.aa34b439.js"
+  }
+]);
